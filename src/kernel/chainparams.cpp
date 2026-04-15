cf0a669a1994484caf9ce6f7c214dc5bfbf2919b
cryptaxe <cryptaxe@gmail.com>
2026-04-15 15:56:58 -0700

    Add DrivechainHeight param to activate difficulty reset

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -79,6 +79,7 @@ class CMainParams : public CChainParams {
 public:
     CMainParams() {
         m_chain_type = ChainType::MAIN;
+        consensus.DrivechainHeight = 0;
         consensus.signet_blocks = false;
         consensus.signet_challenge.clear();
         consensus.nSubsidyHalvingInterval = 210000;
@@ -212,6 +213,7 @@ class CTestNetParams : public CChainParams {
 public:
     CTestNetParams() {
         m_chain_type = ChainType::TESTNET;
+        consensus.DrivechainHeight = 0;
         consensus.signet_blocks = false;
         consensus.signet_challenge.clear();
         consensus.nSubsidyHalvingInterval = 210000;
@@ -321,6 +323,7 @@ class CTestNet4Params : public CChainParams {
 public:
     CTestNet4Params() {
         m_chain_type = ChainType::TESTNET4;
+        consensus.DrivechainHeight = 0;
         consensus.signet_blocks = false;
         consensus.signet_challenge.clear();
         consensus.nSubsidyHalvingInterval = 210000;
@@ -473,6 +476,7 @@ public:
         }
 
         m_chain_type = ChainType::SIGNET;
+        consensus.DrivechainHeight = 0;
         consensus.signet_blocks = true;
         consensus.signet_challenge.assign(bin.begin(), bin.end());
         consensus.nSubsidyHalvingInterval = 210000;
@@ -562,6 +566,7 @@ public:
     explicit CRegTestParams(const RegTestOptions& opts)
     {
         m_chain_type = ChainType::REGTEST;
+        consensus.DrivechainHeight = 0;
         consensus.signet_blocks = false;
         consensus.signet_challenge.clear();
         consensus.nSubsidyHalvingInterval = 150;
