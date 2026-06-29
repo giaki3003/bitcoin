e3d8b583a0244021816e730ad96e332742b05d1c
cryptaxe <cryptaxe@gmail.com>
2026-06-29 16:19:56 -0700

    Update default data dir and conf file name to drivechain-ecash

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -34,7 +34,7 @@
 #include <utility>
 #include <variant>
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
+const char * const BITCOIN_CONF_FILENAME = "drivechain-ecash.conf";
 const char * const BITCOIN_SETTINGS_FILENAME = "settings.json";
 
 ArgsManager gArgs;
@@ -756,19 +756,17 @@ bool HasTestOption(const ArgsManager& args, const std::string& test_option)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows:
-    //   old: C:\Users\Username\AppData\Roaming\Bitcoin
-    //   new: C:\Users\Username\AppData\Local\Bitcoin
-    // macOS: ~/Library/Application Support/Bitcoin
-    // Unix-like: ~/.bitcoin
+    // Windows: C:\Users\Username\AppData\Local\drivechain-ecash
+    // macOS: ~/Library/Application Support/drivechain-ecash
+    // Unix-like: ~/.drivechain-ecash
 #ifdef WIN32
     // Windows
     // Check for existence of datadir in old location and keep it there
-    fs::path legacy_path = GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    fs::path legacy_path = GetSpecialFolderPath(CSIDL_APPDATA) / "drivechain-ecash";
     if (fs::exists(legacy_path)) return legacy_path;
 
     // Otherwise, fresh installs can start in the new, "proper" location
-    return GetSpecialFolderPath(CSIDL_LOCAL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_LOCAL_APPDATA) / "drivechain-ecash";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -778,10 +776,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef __APPLE__
     // macOS
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/drivechain-ecash";
 #else
     // Unix-like
-    return pathRet / ".bitcoin";
+    return pathRet / ".drivechain-ecash";
 #endif
 #endif
 }
